0646c9e7ea9ac2fcf8ae703e361e3c1338ab21b7
hugcorcor <hugocorreacortijo@hotmail.com>
2024-10-24 12:38:52 +0200

    version inical

---
+++ app/src/main/java/com/example/imcapp/Imccalculatormain.kt
@@ -0,0 +1,62 @@
+package com.example.imcapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Imccalculatormain : AppCompatActivity() {
+    private var isMaleSelected:Boolean = false
+    private lateinit var viewMale:CardView
+    private lateinit var viewFemale:CardView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_imccalculatormain)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        initComponents()
+        initListeners()
+        initUI()
+    }
+
+    private fun initUI() {
+        setGenderColor()
+    }
+
+    private fun initListeners() {
+
+        viewMale.setOnClickListener {
+            isMaleSelected=true
+            setGenderColor()}
+        viewFemale.setOnClickListener {
+            isMaleSelected=false
+            setGenderColor()}
+    }
+
+    private fun setGenderColor(){
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(!isMaleSelected))
+    }
+
+    private fun getBackgroundColor(isComponentSelected: Boolean):Int{
+        val colorReference = if(isComponentSelected) {
+            R.color.bg_comp_sel
+        } else {
+            R.color.bg_comp
+        }
+        return ContextCompat.getColor(this,colorReference)
+    }
+
+
+    private fun initComponents() {
+        viewFemale = findViewById(R.id.viewFemale)
+        viewMale = findViewById(R.id.viewMale)
+    }
+}
